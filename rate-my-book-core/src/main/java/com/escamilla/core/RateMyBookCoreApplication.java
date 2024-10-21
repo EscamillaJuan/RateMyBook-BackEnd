84c8dde5765491e3873712187b97d4ba298f3365
EscamillaJuan <juanpedroescamilla@outlook.com>
2024-10-20 21:07:53 -0600

    init project

---
+++ rate-my-book-core/src/main/java/com/escamilla/core/RateMyBookCoreApplication.java
@@ -0,0 +1,13 @@
+package com.escamilla.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateMyBookCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateMyBookCoreApplication.class, args);
+	}
+
+}
