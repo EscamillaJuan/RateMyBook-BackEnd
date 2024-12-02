126850a4e1507bdb219b5fc8bf6a784ae487cfcf
EscamillaJuan <juanpedroescamilla@outlook.com>
2024-12-01 18:52:21 -0600

    feat: add models

---
+++ rate-my-book-user-service/src/main/java/com/escamilla/user_service/models/entity/Role.java
@@ -0,0 +1,6 @@
+package com.escamilla.user_service.models.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- rate-my-book-user-service/src/main/java/com/escamilla/user_service/models/entity/UserEntity.java
+++ rate-my-book-user-service/src/main/java/com/escamilla/user_service/models/entity/UserEntity.java
@@ -3,27 +3,71 @@ package com.escamilla.user_service.models.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 @Entity
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table
-public class UserEntity {
+@Table(name = "_user")
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    @Column(unique = true)
     private String email;
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
-    private String role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
     private String userName;
     private String name;
     private String lastName;
     private String profilePictureUri;
-    private String dateBirth;
+    private Date dateBirth;
     private String gender;
     private String[] favoriteTopics;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
